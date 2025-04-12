4c94c8a1c99883c4bb65ec84754e70210e2c8e2d
Kevin Zhang <kevinzhang@dhcp-10-172-245-96.umd.edu>
2025-04-12 07:19:25 -0400

    Camera In-Progress

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,53 +1,54 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { Image, StyleSheet, Platform } from "react-native";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeScreen() {
   return (
     <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+          source={require("@/assets/images/partial-react-logo.png")}
+          style={styles.logo}
         />
-      }>
+      }
+    >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+        <ThemedText type="title">Medicine Bag</ThemedText>
       </ThemedView>
+
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">Dashboard</ThemedText>
+
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel
+          laoreet orci.
         </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+
         <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
+          Morbi hendrerit, leo non suscipit congue, mi neque vulputate est, at
+          cursus nunc magna vitae quam. Pellentesque vulputate vel elit id
+          dignissim. Praesent laoreet tellus ut accumsan ultrices. Ut vel lorem
+          in justo luctus fringilla. Sed varius mi eu lorem vestibulum
+          tristique.
         </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+
         <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+          Curabitur vestibulum vulputate ex, et consectetur sapien porta id.
+          Vivamus elementum mollis sapien, ut faucibus eros tincidunt sit amet.
+          Aliquam id enim quis leo tempus ultricies. Nunc tristique, dolor non
+          imperdiet venenatis, magna orci imperdiet nibh, ut condimentum lorem
+          tortor sed lorem.
+        </ThemedText>
+
+        <ThemedText>
+          Phasellus non odio sodales, suscipit risus sit amet, suscipit nisl.
+          Mauris bibendum volutpat arcu eu dapibus. Proin vehicula lacinia
+          rutrum. Pellentesque quis sapien semper, dictum purus sit amet,
+          porttitor erat. Donec in viverra felis. Interdum et malesuada fames ac
+          ante ipsum primis in faucibus.
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
@@ -56,19 +57,19 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
     gap: 8,
-    marginBottom: 8,
+    marginBottom: 16,
   },
-  reactLogo: {
+  logo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
